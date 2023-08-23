06e9b6913b822216a54904a6902c97a80562e007
aklaran <botbunk@gmail.com>
2023-08-23 17:00:50 -0400

    Connects a route to mongodb

---
+++ src/app/api/route.ts
@@ -0,0 +1,15 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("dev");
+
+    const movies = await db.collection("tricks").find({}).limit(10).toArray();
+
+    return NextResponse.json(movies);
+  } catch (e) {
+    console.error(e);
+  }
+}
