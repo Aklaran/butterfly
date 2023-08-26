95cb959f4294b42a4b600b601074c9e3864479e2
Bo Tembunkiart <Aklaran@users.noreply.github.com>
2023-08-26 13:47:35 -0400

    Creates Trick Index and Trick Detail pages (#14)
    * retrieves and displays tricks on index page
    
    * uses dashes for trick names (cuz they're PKs)
    
    * CreatesTrickDetailPage
    
    * task management in code lmao

---
+++ src/app/tricks/[trickName]/page.tsx
@@ -0,0 +1,36 @@
+import { ParsedUrlQuery } from 'querystring';
+import React from 'react';
+
+import styles from './page.module.css';
+import TrickController from '@/controllers/trick-controller';
+
+interface TrickDetailPageProps {
+	params: ParsedUrlQuery;
+}
+
+export default async function TrickDetailPage({
+	params,
+}: TrickDetailPageProps) {
+	const controller = new TrickController();
+
+	const { trickName } = params;
+
+	if (typeof trickName !== 'string') {
+		throw new Error(
+			`TrickDetailPage received invalid URL parameter: ${trickName}`
+		);
+	}
+	const trick = await controller.getTrick(trickName);
+
+	return (
+		<div className={styles.wrapper}>
+			<h1>{trick.name}</h1>
+			<h2>landing stances:</h2>
+			<ul>
+				{trick.landingStances.map((stance) => (
+					<li key={stance}>{stance}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
---
+++ src/components/trick-table-item/trick-table-item.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Trick from '@/models/trick/trick';
+
+import styles from './trick-table-item.module.css';
+import Link from 'next/link';
+
+export default function TrickTableItem({
+	trick,
+	key,
+}: {
+	trick: Trick;
+	key: string;
+}) {
+	return (
+		<li key={key} className={styles.wrapper}>
+			<Link href={`/tricks/${trick.name}`}>
+				<p>{trick.name}</p>
+				<input type='checkbox' />
+			</Link>
+		</li>
+	);
+}
+
+// TODO: When I get back, gotta make the TrickDetail page and link to it in the listitems DONE
+// then merge this pr and rebase the UserTrick pr
+// then wire up the UserTrick controller
+// then figure out where to place userID and default trickset generation in the app
+// then create a TrickAnnotationService to overlay the UserTrick and Trick models to inform the UI
+// ^ that will also require updating the Trick model to have `active` booleans for each entry transition and landing stance
+// then make a computed "active" property of Trick to inform the checkbox display on the index
+// then wire up Update actions on the UserTrickController to the TrickDetail page
