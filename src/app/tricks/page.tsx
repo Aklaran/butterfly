d8a7aeebe28791b0117402049d611b1485d5b349
aklaran <botbunk@gmail.com>
2023-08-28 00:14:56 -0400

    Maybe makes something work with RSCs and React Query??

--- src/app/tricks/page.tsx
+++ src/app/tricks/page.tsx
@@ -1,24 +1,17 @@
-'use client';
 import React from 'react';
 
-import { useQuery } from '@tanstack/react-query';
 import Tricktionary from '@/components/tricktionary/tricktionary';
+import Trick from '@/models/trick/trick';
 
 async function getTricks() {
-	return fetch('api/tricks').then((response) => response.json());
+	return fetch(`${process.env.API_URL}/tricks`).then((response) => {
+		return response.json() as unknown as Trick[];
+	});
 }
 
-export default function TricksList() {
-	const tricksQuery = useQuery({
-		queryKey: ['tricks'],
-		queryFn: getTricks,
-	});
-
-	if (tricksQuery.isLoading) return <h1>loading...</h1>;
-	if (tricksQuery.isError)
-		return <pre>{JSON.stringify(tricksQuery.error)}</pre>;
-
-	const tricks = tricksQuery.data;
+export default async function TricksList() {
+	const tricks = await getTricks();
+	console.log(tricks);
 
 	return <Tricktionary tricks={tricks} />;
 }
--- src/components/tricktionary/tricktionary.tsx
+++ src/components/tricktionary/tricktionary.tsx
@@ -1,25 +1,37 @@
+'use client';
 import React from 'react';
 
 import Trick from '@/models/trick/trick';
 
 import TrickTableItem from '../trick-table-item/trick-table-item';
 import styles from './tricktionary.module.css';
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 interface TricktionaryProps {
 	tricks: Trick[];
 }
 
+async function getTricks() {
+	return fetch('http://localhost:3000/api/tricks').then((response) => {
+		console.log('react queried some tricks');
+		return response.json() as unknown as Trick[];
+	});
+}
+
 export default function Tricktionary({ tricks }: TricktionaryProps) {
-	// const { data } = useQuery({
-	// 	queryKey: ['tricks'],
-	// 	queryFn: controller.getAllTricks,
-	// 	initialData: tricks,
-	// });
+	const tricksQuery = useQuery({
+		queryKey: ['tricks'],
+		queryFn: getTricks,
+		initialData: tricks,
+	});
+
+	if (tricksQuery.isLoading) return <h1>loading...</h1>;
+	if (tricksQuery.isError)
+		return <pre>{JSON.stringify(tricksQuery.error)}</pre>;
 
 	return (
 		<ul className={styles.wrapper}>
-			{tricks.map((trick) => (
+			{tricksQuery.data.map((trick) => (
 				<TrickTableItem trick={trick} key={trick.name} />
 			))}
 		</ul>
