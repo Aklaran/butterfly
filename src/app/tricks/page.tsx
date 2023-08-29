225abfa41e3d6d6df02dd1673c471e7868d82fcd
aklaran <botbunk@gmail.com>
2023-08-29 12:46:39 -0400

    makes /tricks page render dynamically to solve build issues

--- src/app/tricks/page.tsx
+++ src/app/tricks/page.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Tricktionary from '@/components/tricktionary/tricktionary';
 import TrickController from '@/controllers/trick-controller';
 
+export const dynamic = 'force-dynamic';
+
 export default async function TricksList() {
 	const controller = new TrickController();
 	const tricks = await controller.getAllTricks();
