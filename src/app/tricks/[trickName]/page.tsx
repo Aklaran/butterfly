eb1c53aac7329ca6b43c0978992787f126fe3070
aklaran <botbunk@gmail.com>
2023-08-28 22:21:08 -0400

    comments out trickdetail page for now

--- src/app/tricks/[trickName]/page.tsx
+++ src/app/tricks/[trickName]/page.tsx
@@ -1,37 +1,37 @@
-import { ParsedUrlQuery } from 'querystring';
-import React from 'react';
+// import { ParsedUrlQuery } from 'querystring';
+// import React from 'react';
 
-import TrickController from '@/controllers/trick-controller';
+// import TrickController from '@/controllers/trick-controller';
 
-import styles from './page.module.css';
+// import styles from './page.module.css';
 
-interface TrickDetailPageProps {
-	params: ParsedUrlQuery;
-}
+// interface TrickDetailPageProps {
+// 	params: ParsedUrlQuery;
+// }
 
-export default async function TrickDetailPage({
-	params,
-}: TrickDetailPageProps) {
-	const controller = new TrickController();
+// export default async function TrickDetailPage({
+// 	params,
+// }: TrickDetailPageProps) {
+// 	const controller = new TrickController();
 
-	const { trickName } = params;
+// 	const { trickName } = params;
 
-	if (typeof trickName !== 'string') {
-		throw new Error(
-			`TrickDetailPage received invalid URL parameter: ${trickName}`
-		);
-	}
-	const trick = await controller.getTrick(trickName);
+// 	if (typeof trickName !== 'string') {
+// 		throw new Error(
+// 			`TrickDetailPage received invalid URL parameter: ${trickName}`
+// 		);
+// 	}
+// 	const trick = await controller.getTrick(trickName);
 
-	return (
-		<div className={styles.wrapper}>
-			<h1>{trick.name}</h1>
-			<h2>landing stances:</h2>
-			<ul>
-				{trick.landingStances.map((stance) => (
-					<li key={stance}>{stance}</li>
-				))}
-			</ul>
-		</div>
-	);
-}
+// 	return (
+// 		<div className={styles.wrapper}>
+// 			<h1>{trick.name}</h1>
+// 			<h2>landing stances:</h2>
+// 			<ul>
+// 				{trick.landingStances.map((stance) => (
+// 					<li key={stance}>{stance}</li>
+// 				))}
+// 			</ul>
+// 		</div>
+// 	);
+// }
