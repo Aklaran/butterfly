e4377c594af1f68159dda4fcf96a69113f79279e
aklaran <botbunk@gmail.com>
2023-08-29 12:30:03 -0400

    logging

--- src/controllers/trick-controller.ts
+++ src/controllers/trick-controller.ts
@@ -4,6 +4,11 @@ export default class TrickController {
 	constructor() {}
 
 	async getAllTricks() {
+		console.log(
+			'Running getAllTricks() with endpoint:',
+			`${process.env.NEXT_PUBLIC_API_URL}/tricks`
+		);
+
 		const result = await fetch(
 			`${process.env.NEXT_PUBLIC_API_URL}/tricks`
 		).then((response) => {
