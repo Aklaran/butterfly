25e3a584a1b057ef3faf19b5e39921b6ce729644
aklaran <Aklaran@users.noreply.github.com>
2023-09-16 14:19:14 -0400

    feat: Adds middleware to enable CORS
    Middleware used to allow regex testing for vercel-generated preview subdomains

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const allowedOriginPattern =
+	/^https:\/\/trickidex(-([a-zA-Z0-9-]+)?aklaran)?\.vercel\.app$/;
+
+export function middleware(request: NextRequest) {
+	const requestHeaders = new Headers(request.headers);
+	const response = NextResponse.next({
+		request: {
+			headers: requestHeaders,
+		},
+	});
+
+	const origin = requestHeaders.get('origin');
+	if (origin && allowedOriginPattern.test(origin)) {
+		response.headers.set('Access-Control-Allow-Origin', origin);
+		response.headers.set('Access-Control-Allow-Credentials', 'true');
+		response.headers.set(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, DELETE, OPTIONS'
+		);
+		response.headers.set(
+			'Access-Control-Allow-Headers',
+			'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+		);
+	}
+
+	return response;
+}
