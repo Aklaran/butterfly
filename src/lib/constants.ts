1d5271f990bd88205f64c53cba6ef924a1f59aac
aklaran <Aklaran@users.noreply.github.com>
2023-09-18 12:47:00 -0400

    chore: Changes to NEXT_PUBLIC vercel env vars

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,13 +1,10 @@
 let url = 'http://localhost:3000/api';
 
-console.log(process.env.VERCEL_ENV);
-console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
-
-if (process.env.VERCEL_ENV == 'development') {
+if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'development') {
 	url = 'http://localhost:3000/api';
-} else if (process.env.VERCEL_ENV == 'preview') {
-	url = `https://${process.env.VERCEL_BRANCH_URL}/api`;
-} else if (process.env.VERCEL_ENV == 'production') {
+} else if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'preview') {
+	url = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api`;
+} else if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'production') {
 	url = `https://${process.env.STATIC_URL}/api`;
 }
 
