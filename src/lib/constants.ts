38bde17940cd00150394e60fc56e9e40d8ffae66
aklaran <Aklaran@users.noreply.github.com>
2023-09-18 13:11:20 -0400

    copout: Fuck environment variables

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,12 @@
 let url = 'http://localhost:3000/api';
 
-console.log(process.env);
-console.log(process.env.STATIC_URL);
-
 if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'development') {
 	url = 'http://localhost:3000/api';
 } else if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'preview') {
 	url = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api`;
 } else if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'production') {
-	url = `https://${process.env.STATIC_URL}/api`;
+	// FIXME: use STATIC_URL environment variable for prod API URL
+	url = 'https://trickidex.vercel.app/api';
 }
 
 export const API_URL = url;
