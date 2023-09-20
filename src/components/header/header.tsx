9e7d4d97b11ecd8ee95ceb9dbccc42235da1e54d
aklaran <Aklaran@users.noreply.github.com>
2023-09-19 20:47:50 -0400

    feat: Styles header with Tailwind

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,15 +8,17 @@ export default async function Header() {
 	const session = await getServerSession(authOptions);
 
 	return (
-		<header>
-			<h1>
-				<Link href='/'>TRICKIDEX</Link>
+		<header className='h-[100px] w-[100%] bg-primary flex flex-col items-center justify-center'>
+			<h1 className='text-primary-foreground text-lg font-bold'>
+				<Link href='/'>BUTTERFLY</Link>
 			</h1>
 			{session ? (
-				<>
+				<div className='text-primary-foreground text-center'>
 					<p>Hello {session.user?.name}!</p>
-					<Link href={'/api/auth/signout'}>Log Out</Link>
-				</>
+					<Link className='color-gray' href={'/api/auth/signout'}>
+						Log Out
+					</Link>
+				</div>
 			) : (
 				<Link href={'/api/auth/signin'}>Sign in</Link>
 			)}
