f8388f6299d4271824c59efb8e20ec0c0ca25ef1
aklaran <Aklaran@users.noreply.github.com>
2023-09-19 20:21:53 -0400

    feat: Add link to trick detail page in table

--- src/components/tricktionary/columns.tsx
+++ src/components/tricktionary/columns.tsx
@@ -3,18 +3,32 @@ import React from 'react';
 import { ColumnDef } from '@tanstack/react-table';
 import AnnotatedTrick from '@/models/annotated-trick/annotated-trick';
 import { Checkbox } from '../ui/checkbox';
+import Link from 'next/link';
+import { Edit } from 'lucide-react';
 
 export const columns: ColumnDef<AnnotatedTrick>[] = [
-	{
-		accessorKey: 'name',
-		header: 'Name',
-	},
 	{
 		id: 'isActive',
 		cell: ({ row }) => {
 			const trick = row.original;
 
 			return <Checkbox checked={trick.isActive()} />;
+		},
+	},
+	{
+		accessorKey: 'name',
+		header: 'Name',
+	},
+	{
+		id: 'edit',
+		cell: ({ row }) => {
+			const trick = row.original;
+
+			return (
+				<Link href={`/tricks/${trick.name}`}>
+					<Edit size={20} />
+				</Link>
+			);
 		},
 	},
 ];
