bf18acd70dd7dd3c61cc0dcb51ccf35175199e71
aklaran <Aklaran@users.noreply.github.com>
2023-09-19 22:45:31 -0400

    feat: Adds a form for the combo gen

--- src/components/combo-generator/combo-generator.tsx
+++ src/components/combo-generator/combo-generator.tsx
@@ -8,6 +8,26 @@ import generateCombos from '@/services/combo-generation-service';
 import { DataTable } from '@/components/ui/data-table';
 import { columns } from '@/components/combo-generator/columns';
 import { Combo } from '@/models/combo/combo';
+import { Button } from '../ui/button';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
 
 export default function ComboGenerator({
 	tricks,
@@ -24,12 +44,21 @@ export default function ComboGenerator({
 
 	const [numPossibleCombos, setNumPossibleCombos] = React.useState(0);
 	const [generatedCombos, setGeneratedCombos] = React.useState<Combo[]>([]);
-	const [targetLength, setTargetLength] = React.useState(3);
 
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
+	const formSchema = z.object({
+		length: z.number().min(0).max(10),
+	});
 
-		const combos = generateCombos(annotatedTricks, targetLength);
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			length: 3,
+		},
+	});
+
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		const { length } = values;
+		const combos = generateCombos(annotatedTricks, length);
 		// TODO: inject interface ComboSelector
 		const selector = new RandomComboSelector(combos);
 		const selectedCombos = selector.take(5);
@@ -47,17 +76,64 @@ export default function ComboGenerator({
 
 	return (
 		<div>
-			<div className='wrapper'>
-				<form onSubmit={handleSubmit}>
-					<button>Generate!</button>
-				</form>
-			</div>
 			<div>
-				<h1 className='text-lg font-bold'>
-					Combos (showing 5 of {numPossibleCombos} possible)
-				</h1>
-				<DataTable columns={columns} data={generatedCombos} />
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className='space-y-8'
+					>
+						<FormField
+							control={form.control}
+							name='length'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Length</FormLabel>
+									<FormControl>
+										<Select
+											onValueChange={(value) =>
+												field.onChange(Number(value))
+											}
+											defaultValue={field.value.toString()}
+										>
+											<SelectTrigger className='w-[180px]'>
+												<SelectValue placeholder='Length' />
+											</SelectTrigger>
+											<SelectContent>
+												<SelectItem value='2'>
+													2
+												</SelectItem>
+												<SelectItem value='3'>
+													3
+												</SelectItem>
+												<SelectItem value='4'>
+													4
+												</SelectItem>
+												<SelectItem value='5'>
+													5 (this will crash the app)
+												</SelectItem>
+											</SelectContent>
+										</Select>
+									</FormControl>
+									<FormDescription>
+										The length of combos you want to
+										generate.
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type='submit'>Generate</Button>
+					</form>
+				</Form>
 			</div>
+			{generatedCombos.length > 0 && (
+				<div>
+					<h1 className='text-lg font-bold'>
+						Combos (showing 5 of {numPossibleCombos} possible)
+					</h1>
+					<DataTable columns={columns} data={generatedCombos} />
+				</div>
+			)}
 		</div>
 	);
 }
