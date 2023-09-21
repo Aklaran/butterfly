3b763ae27c23c4732158dedc1cc9b1e17b72a87e
aklaran <Aklaran@users.noreply.github.com>
2023-09-20 22:35:22 -0400

    fix: Gets UserTricks from the client in the Index.
    This is not ideal, but it works better for now.
    plz Next, come out with better ways to revalidate server-side data  🙏

--- src/app/tricks/page.tsx
+++ src/app/tricks/page.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
 import Tricktionary from '@/components/tricktionary/tricktionary';
-import {
-	fetchAllTrickData,
-	fetchAllUserTrickData,
-} from '@/services/query-service';
-import TrickAnnotationService from '@/services/trick-annotation-service';
+import { fetchAllTrickData } from '@/services/query-service';
 
 export const dynamic = 'force-dynamic';
 
 export default async function TricksList() {
 	const tricks = await fetchAllTrickData();
-	const userTricks = await fetchAllUserTrickData();
-	const annotatedTricks = new TrickAnnotationService()
-		.annotateTricks(tricks, userTricks)
-		.map((at) => at.data);
 
-	return <Tricktionary annotatedTrickData={annotatedTricks} />;
+	return <Tricktionary baseTrickData={tricks} />;
 }
--- src/components/tricktionary/tricktionary.tsx
+++ src/components/tricktionary/tricktionary.tsx
@@ -3,21 +3,38 @@ import React from 'react';
 
 import { DataTable } from '../ui/data-table';
 import { columns } from './columns';
-import AnnotatedTrick, {
-	AnnotatedTrickData,
-} from '@/models/annotated-trick/annotated-trick';
+import AnnotatedTrick from '@/models/annotated-trick/annotated-trick';
+import TrickAnnotationService from '@/services/trick-annotation-service';
+import { TrickData } from '@/models/trick/trick';
+import { useQuery } from '@tanstack/react-query';
+import UserTrickController from '@/controllers/user-trick-controller';
 
 // FIXME: Can I invalidate React Cache on a trick mutation instead?
 // FIXME: Doesn't rerender when navigating to the page with the back button
 export const dynamic = 'force-dynamic';
 
 interface TricktionaryProps {
-	annotatedTrickData: AnnotatedTrickData[];
+	baseTrickData: TrickData[];
 }
 
-export default function Tricktionary({
-	annotatedTrickData,
-}: TricktionaryProps) {
+export default function Tricktionary({ baseTrickData }: TricktionaryProps) {
+	const userTrickController = new UserTrickController();
+
+	const userTrickQuery = useQuery({
+		queryKey: ['user-tricks'],
+		queryFn: () => userTrickController.getAllUserTricks(),
+	});
+
+	if (userTrickQuery.isLoading) {
+		return <p>loading...</p>;
+	}
+
+	if (userTrickQuery.isError)
+		return `An error occurred: ${userTrickQuery.error}`;
+
+	const annotatedTrickData = new TrickAnnotationService()
+		.annotateTricks(baseTrickData, userTrickQuery.data)
+		.map((at) => at.data);
 	const annotatedTricks = annotatedTrickData.map((data) =>
 		AnnotatedTrick.fromData(data)
 	);
