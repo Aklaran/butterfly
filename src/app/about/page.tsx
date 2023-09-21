5b51adbf2d395b52de85b43cf310ad451841bd23
aklaran <Aklaran@users.noreply.github.com>
2023-09-21 12:00:52 -0400

    feat: Add onboarding in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,49 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function About() {
 	return (
-		<p>
-			Martial Arts Tricking is an aesthetic blend of kicks, flips, and
-			twists that a practitioner chains together in “combos” to create a
-			unique art all it’s own. Sampling and using movements from martial
-			arts, break dancing, and tumbling, tricking has many different
-			styles.
-		</p>
+		<div className='p-6'>
+			<h1 className='text-2xl font-semibold mb-4'>
+				Welcome to Butterfly!
+			</h1>
+			<p className='mb-2'>
+				Track your tricks and generate endless combos.
+			</p>
+			<p className='mb-2'>
+				Visit the{' '}
+				<Link href='/tricks' className='text-bordeaux underline'>
+					Tricktionary
+				</Link>{' '}
+				to see your tricks.
+			</p>
+			<p className='mb-2'>
+				Edit a trick to set transitions and landing stances.
+			</p>
+			<p className='mb-2'>
+				Got some tricks? Try the{' '}
+				<Link className='text-bordeaux underline' href='/combo-gen'>
+					Combo Generator
+				</Link>
+				.
+			</p>
+			<p className='mb-2'>
+				Set the combo length, hit Generate, and let it rip!
+			</p>
+			<p className='mb-2'>
+				Find a bug or got feedback?{' '}
+				<a
+					className='text-bordeaux underline'
+					href='https://instagram.com/botbunk'
+				>
+					DM me
+				</a>
+				.
+			</p>
+			<p className='mt-4'>
+				Thanks for joining Butterfly. Happy tricking, and keep loving
+				it!
+			</p>
+		</div>
 	);
 }
