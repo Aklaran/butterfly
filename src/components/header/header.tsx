c6b9d87778b3f3a106934b277a3c37d9cccdada3
aklaran <Aklaran@users.noreply.github.com>
2023-09-22 10:09:26 -0400

    feat: Add nav sheet
    Site logo is off-center if you're not logged in but that's a future problem

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,32 +3,81 @@ import React from 'react';
 
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
-import { Info } from 'lucide-react';
+import { Info, Menu } from 'lucide-react';
+
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
+
+const pages = [
+	{
+		label: 'Tricktionary',
+		route: '/tricks',
+	},
+	{
+		label: 'Combo Generator',
+		route: '/combo-gen',
+	},
+	{
+		label: 'About',
+		route: '/about',
+	},
+];
 
 export default async function Header() {
 	const session = await getServerSession(authOptions);
 
 	return (
 		<header className='h-[100px] w-[100%] bg-primary'>
-			<h1 className='text-primary-foreground text-3xl text-center font-bold fixed inset-0 top-7'>
-				<Link href='/'>BUTTERFLY</Link>
-			</h1>
 			<div className='flex items-center justify-between px-5 h-[100%]'>
 				<Link href='/about'>
 					<Info color='white' />
 				</Link>
 
+				<h1 className='text-primary-foreground text-3xl text-center font-bold'>
+					<Link href='/'>BUTTERFLY</Link>
+				</h1>
+
 				<div className='text-primary-foreground text-center'>
 					{session ? (
-						<>
-							<p>Hello {session.user?.name}!</p>
-							<Link
-								className='color-gray'
-								href={'/api/auth/signout'}
-							>
-								Log Out
-							</Link>
-						</>
+						<Sheet>
+							<SheetTrigger>
+								<Menu />
+							</SheetTrigger>
+							<SheetContent className='w-[200px]'>
+								<SheetHeader>
+									<SheetTitle className='text-left'>
+										{/* FIXME: Boy oh boy is that a lot of ! */}
+										Hi {session.user!.name!.split(' ')[0]}!
+									</SheetTitle>
+								</SheetHeader>
+								<ul className='h-[80%] flex flex-col justify-center gap-8 items-start'>
+									{pages.map(({ label, route }) => (
+										<li key={route}>
+											<SheetClose asChild>
+												<Link href={route}>
+													{label}
+												</Link>
+											</SheetClose>
+										</li>
+									))}
+								</ul>
+								<SheetFooter>
+									<Link
+										className='text-gray-600'
+										href='/api/auth/signout'
+									>
+										Sign Out
+									</Link>
+								</SheetFooter>
+							</SheetContent>
+						</Sheet>
 					) : (
 						<Link href={'/api/auth/signin'}>Sign in</Link>
 					)}
